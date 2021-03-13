c66edded92c88c9bd1c7f60f30b085f2a091cb65
istmr <isct.stefan@gmail.com>
2021-03-13 15:42:04 +0200

    created the landing page

--- frontend/ui/src/App.js
+++ frontend/ui/src/App.js
@@ -1,164 +1,50 @@
 import React from 'react'
 import { Menubar } from 'primereact/menubar';
-
+import { Button } from 'primereact/button';
 
 class App extends React.Component {
-  render() {
-    const items = [
-      {
-          label: 'File',
-          icon: 'pi pi-fw pi-file',
-          items: [
-              {
-                  label: 'New',
-                  icon: 'pi pi-fw pi-plus',
-                  items: [
-                      {
-                          label: 'Bookmark',
-                          icon: 'pi pi-fw pi-bookmark'
-                      },
-                      {
-                          label: 'Video',
-                          icon: 'pi pi-fw pi-video'
-                      },
+    render() {
+        const items = [
+            {
+                label: 'Home',
+            },
+            {
+                label: 'Graphs',
+            },
+            {
+                label: 'Predict',
+            }
+        ];
 
-                  ]
-              },
-              {
-                  label: 'Delete',
-                  icon: 'pi pi-fw pi-trash'
-              },
-              {
-                  separator: true
-              },
-              {
-                  label: 'Export',
-                  icon: 'pi pi-fw pi-external-link'
-              }
-          ]
-      },
-      {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
-          items: [
-              {
-                  label: 'Left',
-                  icon: 'pi pi-fw pi-align-left'
-              },
-              {
-                  label: 'Right',
-                  icon: 'pi pi-fw pi-align-right'
-              },
-              {
-                  label: 'Center',
-                  icon: 'pi pi-fw pi-align-center'
-              },
-              {
-                  label: 'Justify',
-                  icon: 'pi pi-fw pi-align-justify'
-              },
+        return (
+            <div >
+                <Menubar model={items} end={
+                    <Button label="Sign In" className="header-button p-button-text" icon="pi pi-google" />
+                } />
 
-          ]
-      },
-      {
-          label: 'Users',
-          icon: 'pi pi-fw pi-user',
-          items: [
-              {
-                  label: 'New',
-                  icon: 'pi pi-fw pi-user-plus',
 
-              },
-              {
-                  label: 'Delete',
-                  icon: 'pi pi-fw pi-user-minus',
+                <div className="p-grid p-m-2 p-justify-center p-align-center">
+                    <div className='p-col-0 p-md-2  ' />
+                    <div className='p-col-0 p-md-3  '>
+                        <div className='landing-primary-text'>
+                            Predict a Heart Disease
+                        </div>
+                        <div className='landing-secondary-text'>
+                            Sign in to have access to your patients and your previous diagnostics.
+                        </div>
+                        <Button label="Sign in with Google" className="p-button-lg p-button-rounded landing-auth-button" icon="pi pi-google" />
+                    </div>
+                    <div className='p-col-12 p-md-6 '>
+                        <img src={process.env.PUBLIC_URL + 'Doc.svg'} className={'landing-image'} height={10} />
+                    </div>
+                    <div className='p-col-0 p-md-1  ' />
 
-              },
-              {
-                  label: 'Search',
-                  icon: 'pi pi-fw pi-users',
-                  items: [
-                      {
-                          label: 'Filter',
-                          icon: 'pi pi-fw pi-filter',
-                          items: [
-                              {
-                                  label: 'Print',
-                                  icon: 'pi pi-fw pi-print'
-                              }
-                          ]
-                      },
-                      {
-                          icon: 'pi pi-fw pi-bars',
-                          label: 'List'
-                      }
-                  ]
-              }
-          ]
-      },
-      {
-          label: 'Events',
-          icon: 'pi pi-fw pi-calendar',
-          items: [
-              {
-                  label: 'Edit',
-                  icon: 'pi pi-fw pi-pencil',
-                  items: [
-                      {
-                          label: 'Save',
-                          icon: 'pi pi-fw pi-calendar-plus'
-                      },
-                      {
-                          label: 'Delete',
-                          icon: 'pi pi-fw pi-calendar-minus'
-                      }
-                  ]
-              },
-              {
-                  label: 'Archieve',
-                  icon: 'pi pi-fw pi-calendar-times',
-                  items: [
-                      {
-                          label: 'Remove',
-                          icon: 'pi pi-fw pi-calendar-minus'
-                      }
-                  ]
-              }
-          ]
-      },
-      {
-          label: 'Quit',
-          icon: 'pi pi-fw pi-power-off'
-      }
-  ];
-  
-  const start = <img alt="logo" src="showcase/images/logo.png" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} height="40" className="p-mr-2"></img>;
-  const startItems = [start,start]
+                    {/* <div className='p-col-0 p-md-1'></div> */}
+                </div>
 
-    return (
-      <div >
-        <Menubar model={items} start={startItems} />
-
-        <div className="p-grid">
-          <div className='p-col-0 p-md-1'></div>
-          <div className='p-col-12 p-md-10'>
-            testasdasdasdasdasdasdasdsatestasdasdasdasdasdasdasdsatestasdasdasdasdasdasdasdsatestasdav
-            sdasdasdasdasdasdsatestasdasdasdasdasdasdasdsa
-            sdasdasdasdasdasdsatestasdasdasdasdasdasdasdsa
-            sdasdasdasdasdasdsatestasdasdasdasdasdasdasdsa
-            sdasdasdasdasdasdsatestasdasdasdasdasdasdasdsa
-            sdasdasdasdasdasdsatestasdasdasdasdasdasdasdsa
-            sdasdasdasdasdasdsatestasdasdasdasdasdasdasdsa
-            sdasdasdasdasdasdsatestasdasdasdasdasdasdasdsa
-            sdasdasdasdasdasdsatestasdasdasdasdasdasdasdsa
-
-          </div>
-          <div className='p-col-0 p-md-1'></div>
-        </div>
-
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 }
 
 export default App;
