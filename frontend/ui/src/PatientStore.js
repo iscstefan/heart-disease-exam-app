abea8baf90f68104ba59466071779b3c3cd12d0b
istmr <isct.stefan@gmail.com>
2021-03-17 17:48:30 +0200

    updated patients route; added datatable

--- frontend/ui/src/PatientStore.js
+++ frontend/ui/src/PatientStore.js
@@ -3,7 +3,37 @@ import constants from './constants.js';
 const SERVER = constants.server_url;
 
 class PatientStore {
-    
+    constructor(user) {
+        this.user = user;
+        this.data = [];
+        this.emitter = new EventEmitter();
+    }
+
+    async getPatients() {
+        try {
+            const response = await fetch(`${SERVER}/users/${this.user.id}/patients`, {
+                method: 'get',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'token': `${this.user.token}`
+                }
+            });
+            const data = await response.json();
+            this.data = data;
+
+            if (response.status === 200) {
+                this.emitter.emit('GET_EXPERIENCES_SUCCESS');
+            } else if (response.status === 401) {
+                this.emitter.emit('UNAUTHORIZED');
+            } else {
+                this.emitter.emit('GET_EXPERIENCES_FAILED');
+            }
+
+        } catch (err) {
+            console.warn(err);
+            this.emitter.emit('GET_EXPERIENCES_FAILED');
+        }
+    }
 }
 
 export default PatientStore;
--- frontend/ui/src/Patients.js
+++ frontend/ui/src/Patients.js
@@ -4,25 +4,90 @@ import { withRouter } from 'react-router';
 import authStore from './AuthStore.js';
 import constants from './constants.js';
 import CustomMenuBar from './CustomMenuBar'
+import { TabView, TabPanel } from 'primereact/tabview';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import PatientStore from './PatientStore.js';
+
+/*
+TO DO:
+-de pus intr-o componenta noua tabelul
+-buton adaugare
+-selectie
+
+*/
 
 class Patients extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            patients: [],
+            page: 0
+        }
+
+        this.store = new PatientStore(this.props.user);
     }
 
     componentDidMount() {
         if (!authStore.user) {
             this.props.history.push('/');
         }
+
+        this.store.getPatients();
+
+        this.store.emitter.addListener('GET_EXPERIENCES_SUCCESS', () => {
+            this.setState({
+                patients: this.store.data
+            })
+        });
+
+        this.store.emitter.addListener('UNAUTHORIZED', () => {
+            this.props.history.push('/')
+        });
+
     }
 
     render() {
+        const codeBodyTemplate = (rowData) => {
+            return (
+                <React.Fragment>
+                    <span className="p-column-title">Firstname</span>
+                    {rowData.firstname}
+                </React.Fragment>
+            );
+        }
+
         return (
             <div>
                 <CustomMenuBar user={this.props.user} />
-                <div class='title-bar p-d-flex p-flex-column p-jc-center p-ai-center'>
-                        {/* <img src={process.env.PUBLIC_URL + 'Doctor.svg'} className={'text-bar'} alt='' width={350}/> */}
-                        <span>Your Patients</span>
+                <div className='title-bar p-d-flex p-flex-column p-jc-center p-ai-center'>
+                    {/* <img src={process.env.PUBLIC_URL + 'Doctor.svg'} className={'text-bar'} alt='' width={350}/> */}
+                    <span>Your Patients</span>
+                </div>
+                <div>
+                    <TabView className={'p-d-flex p-flex-column p-jc-center p-ai-center p-mt-4'}>
+                        <TabPanel header="Patients">
+                            {
+                                this.state.patients.length > 0 &&
+                                <div style={{ width: '70vw' }} className={'datatable-responsive-demo'} >
+                                    <div className="card">
+                                        <DataTable value={this.state.patients} className={'p-datatable-responsive-demo p-mt-4'}
+                                            paginator={this.state.patients.length >= 10} rows={10} first={this.state.page} onPage={(e) => this.setState({ page: e.first })}
+                                            resizableColumns columnResizeMode="expand">
+                                            <Column field="firstname" header="Firstname" body={codeBodyTemplate}></Column>
+                                            <Column field="lastname" header="Lastname"></Column>
+                                            <Column field="email" header="Email" style={{width:'35%'}}></Column>
+                                            <Column field="identification_number" header="ID number"></Column>
+                                        </DataTable>
+                                    </div>
+                                </div>
+                            }
+                        </TabPanel>
+                        <TabPanel header="Graphs">
+                            Content II
+                         </TabPanel>
+                    </TabView>
                 </div>
             </div>
         )
